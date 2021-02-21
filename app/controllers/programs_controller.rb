2605ea19cae13c493ae56ce89f77e5829208e615
Gurpreet Dhaliwal <coderalert0@gmail.com>
2021-02-21 17:02:49 -0500

    More visual improvements

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -12,7 +12,7 @@ class ProgramsController < ApplicationController
     @matching_programs = @programs.where('minimum_step_1_score <= ?', current_user.step_1_score.to_s)
                                   .where('minimum_step_2_score <= ?', current_user.step_2ck_score || 300)
                                   .where("#{current_user.img_type} > ?", 0)
-                                  .where("#{current_user.visa} = ?", 'Yes')
+                                  .where("#{current_user.visa} = ?", 'Yes') # handle situation where user doesnt need visa
                                   .order("#{current_user.img_type} DESC")
 
     bookmark_count = ProgramUser.where(program: @matching_programs, user: current_user, bookmark: true).count
